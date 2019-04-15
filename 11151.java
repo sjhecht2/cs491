ad76305723ca7c7c8b8cbfa56b42cacd19f456ef
Spencer Hecht <sjhecht2@illinois.edu>
2019-04-15 09:14:01 -0500

    finished

--- 11151.java
+++ 11151.java
@@ -1,45 +1,27 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        if(s.equals("")) {
-            return "";
-        }
-        int longest = 1;
-        int start = 0;
-        int end = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
-            int[] temp = expandCenter(s, i, i);
-            int[] temp2 = expandCenter(s, i, i + 1);
-            if((temp[1] - temp[0]) + 1 > longest) {
-                start = temp[0];
-                end = temp[1];
-                longest = temp[1] - temp[0] + 1;
-            }
-            if((temp2[1] - temp2[0]) + 1 > longest) {
-                start = temp2[0];
-                end = temp2[1];
-                longest = temp2[1] - temp2[0] + 1;
-            }
-        }
-        return s.substring(start, end+1);
-    }
-    
-    public int[] expandCenter(String s, int i, int j) {
-        int[] ret = new int[2];
-        if(i != j && s.charAt(i) != s.charAt(j)) {
-            ret[0] = 0;
-            ret[1] = 0;
-            return ret;
-        }
-        while(i >= 0 && j < s.length()) {
-            if(s.charAt(i) == s.charAt(j)) {
-                i--;
-                j++;
-            } else {
-                break;
-            }
-        }
-        ret[0] = i + 1;
-        ret[1] = j - 1;
-        return ret;
-    }
+import java.util.*;
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		String line;
+		while((line = sc.nextLine()) != null) {
+			int n = line.length();
+			int i, j, sub;
+			int mem[][] = new int[n][n];
+			
+			for (i = 0; i < n; i++) {
+				mem[i][i] = 1;
+			}
+			for (sub = 2; sub <= n; sub++) {
+				for (i = 0; i < n - sub + 1; i++) {
+					j = i + sub - 1;
+					
+					if (line.charAt(i) == line.charAt(j) && sub == 2) mem[i][j] = 2;
+					else if (line.charAt(i) == line.charAt(j)) mem[i][j] = mem[i+1][j-1] + 2;
+					else mem[i][j] = (mem[i][j-1] > mem[i+1][j]) ? mem[i][j-1] : mem[i+1][j];
+				}
+			}
+			System.out.println(mem[0][n-1]);
+		}
+	}
 }
